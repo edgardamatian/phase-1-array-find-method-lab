969972641c119dbc30c4d2e08ec2012fd6458bd4
Edgar Damatian <damatian.e@gmail.com>
2024-02-04 13:20:11 -0800

    done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(record) {
+    const winObject = record.find(obj => obj.result === "W");
+    if (winObject) {
+      return winObject.year;
+    } else {
+      return undefined;
+    }
+  }
